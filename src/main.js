6855c5178dc97116375700f7f6f72cc21b19c6f4
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-11 09:06:43 -0700

    checked the basic API call and created a literal for LCC for future input

--- src/main.js
+++ src/main.js
@@ -3,4 +3,35 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
-$(document).ready(function () {});
+$(document).ready(function () {
+  $("#weatherLocation").click(function () {
+    const LCC = $("#CurrencyFrom").val();
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${LCC}`;
+    console.log(process.env.API_KEY);
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+        console.log(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $(".exchageFrom").text(`You requested information on ${response.name}.`);
+      $(".ExchangeTo").text(
+        `The humidity in ${zipcode} is ${response.main.humidity}%`
+      );
+      $(".showRates").text(
+        `The temperature in fahrenheit is  ${temp(response.main.temp)} degrees.`
+      );
+      $(".result").text();
+      $(".Error-Type").text();
+    }
+  });
+});
