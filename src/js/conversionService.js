a8d74fdde35994fefe075541cd7d0fa16e81427a
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-11 13:48:15 -0700

    updating code for cleaner UI and building out a conversion Service

---
+++ src/js/conversionService.js
@@ -0,0 +1,15 @@
+export default class ConversionService {
+  static async getConversion(LLC) {
+    try {
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${LLC}`
+      );
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import ConversionService
 
 $(document).ready(function () {
   $("#Exchange").submit(function () {
