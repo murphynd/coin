bdfc541ef570ced255b383e3d35575ea755ef9ce
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-11 11:25:40 -0700

    this removes second choice boxes from from. working out how to pull conversion

--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ $(document).ready(function () {
   $("#Exchange").submit(function () {
     event.preventDefault();
     const LCC = $("#LLC").val();
-    let llcCon = $("#con").val();
     let numinput = $("#numinput").val();
     console.log(numinput);
-    console.log(llcCon);
+
     console.log(LLC);
     $(".num").text(numinput);
 
@@ -31,10 +30,10 @@ $(document).ready(function () {
 
     function getElements(response) {
       $(".LLC-choice").text(`${response.base_code}.`);
-      $(".LLC-conversion").text(`${response.conversion_rates.AUD}`);
+      $(".LLC-conversion").text(`${response.conversion_rates.USD}`);
       $(".con-title").text("AUD");
       $(".date").text(`${response.time_last_update_utc}`);
-      // $(".showRates").text(`${response.conversion_rates}`);
+      $(".showRates").text(`${response.conversion_rates}`);
       $(".result").text(`${response.result}.`);
       $(".author").text(`${response.terms_of_use}`);
     }
